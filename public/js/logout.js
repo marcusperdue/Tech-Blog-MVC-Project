ef06c86e0464d2eb57e7b275ab6134aef481970c
Marcus Perdue <marcusperduee@gmail.com>
2023-10-29 11:11:08 -0700

    updated views login logout signup dashboard

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,17 +1,25 @@
-async function logout() {
+ document.addEventListener('DOMContentLoaded', () => {
+    const logoutButton = document.querySelector('#logout');
+  
+    if (logoutButton) {
+      logoutButton.addEventListener('click', logout);
+    }
+  });
+  
+
+  async function logout() {
     console.log('Logout button clicked');  
     const response = await fetch('/api/users/logout', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
-
+  
     if (response.ok) {
-        document.location.replace('/');
+      document.location.replace('/');
     } else {
-        alert(response.statusText);
+      alert(response.statusText);
     }
-}
-
-document.querySelector('#logout').addEventListener('click', logout);
+  }
+  
