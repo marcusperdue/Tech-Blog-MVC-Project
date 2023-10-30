3b40e6e43ec5d58bd1f6544ce1efd40dd8c64014
Marcus Perdue <marcusperduee@gmail.com>
2023-10-29 21:28:42 -0700

    heroku server.js

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
+const sequelize = require('../config/connection');
 const {
     Post,
     User,
     Comment
-} = require('../../models');
-const withAuth = require('../../utils/auth');
+} = require('../models');
+const withAuth = require('../utils/auth');
 
 
 router.get('/', withAuth, (req, res) => {
